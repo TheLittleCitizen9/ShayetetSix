fff1c21a6b07e01764b2cfcda6e2f175564e1aa2
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-19 16:03:01 +0300

    Added MisslesLauncher class

---
+++ ShayetetSix/ShayetetSix/MisslesLauncher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShayetetSix
+{
+    public class MisslesLauncher
+    {
+        public Dictionary<int, bool> MisslesFailedLaunchStatus = new Dictionary<int, bool>();
+        public List<IRocket> MissleLauncher = new List<IRocket>();
+
+        public MisslesLauncher()
+        {
+
+        }
+    }
+}

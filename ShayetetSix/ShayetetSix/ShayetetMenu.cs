1ca6f18668bcb723cc9fcf19fb5e0e5b541bab06
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-19 15:58:43 +0300

    Added Validator and Menu classes

---
+++ ShayetetSix/ShayetetSix/ShayetetMenu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MenuBuilder;
+
+namespace ShayetetSix
+{
+    public class ShayetetMenu : BasicMenu<IRocket>
+    {
+        private const string ENTER_VARIABLES = "Enter variables - format X Y";
+        private const string ERROR_MSG = "Invalid input";
+        private readonly List<string> _requaiersInput = new List<string>();
+
+        private Dictionary<string, IActions<IRocket>> _options;
+
+        private Dictionary<string, string> _displayOptions;
+
+
+        public ShayetetMenu()
+        {
+            _options = new Dictionary<string, IActions<IRocket>>();
+            _displayOptions = new Dictionary<string, string>();
+        }
+
+        public override void AddAction(string option, string description, IActions<IRocket> action, bool requiersInput = false)
+        {
+            _options.Add(option, action);
+            _displayOptions.Add(option, description);
+            if (requiersInput)
+            {
+                _requaiersInput.Add(option);
+            }
+        }
+
+        public override void Main()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RunOption(string option, params IRocket[] variables)
+        {
+            _options[option].Action(variables);
+        }
+    }
+}
---
+++ ShayetetSix/ShayetetSix/Validator.cs
@@ -0,0 +1,26 @@
+﻿using MenuBuilder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShayetetSix
+{
+    public class Validator : BasicValidator
+    {
+        public Validator(List<string> options):base(options)
+        {
+
+        }
+
+        public bool ValidateInt(string input)
+        {
+            return int.TryParse(input, out _);
+        }
+
+        public bool ValidateMissleInput(string input)
+        {
+            RocketType rocketType;
+            return Enum.TryParse(input, true, out rocketType);
+        }
+    }
+}
